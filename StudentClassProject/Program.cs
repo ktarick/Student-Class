22cf4fca7a8cd40d69b3059575209a299792957d
Kellan Arick <kellan5015@gmail.com>
2019-02-01 15:24:40 -0500

    Initial Commit

--- StudentClassProject/Program.cs
+++ StudentClassProject/Program.cs
@@ -8,8 +8,74 @@ namespace StudentClassProject
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) //creating instences of students
+        {    
+            var greg = new Student("Greg", "Doud", 3.7);
+            Console.WriteLine(greg.Print());
+            Console.WriteLine(greg.Print("This is a new change."));
+
+            var chris = new Student();
+            chris.FirstName = "Chris";
+            chris.LastName = "Caudill";
+            chris.GPA = 3.5;
+            chris.SAT = 1250;
+            chris.Phone = "555-555-5555";
+            Console.WriteLine(chris.Print());
+            var chrisfullname = chris.FullName();
+
+            var ken = new Student();
+            ken.FirstName = "Ken";
+            ken.LastName = "Doud";
+            ken.GPA = 3.6;
+            ken.SAT = 1300;
+            ken.Phone = "555-555-5555";
+            Console.WriteLine(ken.Print());
+            var kenfullname = ken.FullName();
+
+            var kellan = new Student();
+            kellan.FirstName = "Kellan";
+            kellan.LastName = "Arick";
+            kellan.GPA = 2.5;
+            kellan.SAT = 1150;
+            kellan.Phone = "555-555-5555";
+            Console.WriteLine(kellan.Print());
+            var kellanfullname = kellan.FullName();
+
+            var irena = new Student();
+            irena.FirstName = "Irena";
+            irena.LastName = "Futoransky";
+            irena.GPA = 3.6;
+            irena.SAT = 1375;
+            irena.Phone = "555-555-5555";
+            Console.WriteLine(irena.Print());
+            var irenafullname = irena.FullName();
+
+            var sammy = new Student();
+            sammy.FirstName = "Sammy";
+            sammy.LastName = "Amare";
+            sammy.GPA = 3.3;
+            sammy.SAT = 1400;
+            sammy.Phone = "555-555-5555";
+            Console.WriteLine(sammy.Print());
+            var sammyfullname = sammy.FullName();
+
+            var ben = new Student();
+            ben.FirstName = "Ben";
+            ben.LastName = "Peterson";
+            ben.GPA = 3.0;
+            ben.SAT = 1200;
+            ben.Phone = "555-555-5555";
+            Console.WriteLine(ben.Print());
+            var benfullname = ben.FullName();
+
+            var seth = new Student();
+            seth.FirstName = "Seth";
+            seth.LastName = "Philips";
+            seth.GPA = 2.7;
+            seth.SAT = 1120;
+            seth.Phone = "555-555-5555";
+            Console.WriteLine(seth.Print());
+            var seethfullname = seth.FullName();
         }
     }
 }
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassProject
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string _Phone;
+        public string Phone
+        {
+            get
+            {
+                return _Phone;
+            }
+            set
+            {
+                if (value.Length < 7 || value.Length > 10)
+                    Console.WriteLine("Enter Correct Phone Numeber");
+                else
+                {
+                    this._Phone = value;
+                }
+            }
+            
+        }
+        public string Email { get; set; } // string still allows text
+        public double GPA; //DOUBLE is a floating point number
+        public int SAT;
+        public Student(string fname, string lname, string phone, string email, double gpa, int sat)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Phone = phone;
+            Email = email;
+            GPA = gpa;
+            SAT = sat;
+        }
+        public Student(string fname, string lname, double gpa)
+        {
+            FirstName = "First";
+            LastName = "Last";
+            Phone = "111-111-1111";
+            Email = "blank@blank.com";
+            GPA = 0;
+            SAT = 0;
+        }
+        public Student()
+        {
+        }
+        public string FullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        public string Print()
+        {
+            return $"{FirstName} {LastName}|{Phone}|{Email}|{GPA}|{SAT}";
+        }
+        public string Print(string message)
+        {
+            return $"{message} :: " + Print();
+        }
+        public string Print2()
+        {
+            return FullName() + $"{Phone}|{Email}|{GPA}|{SAT}";
+        }
+    }
+}
